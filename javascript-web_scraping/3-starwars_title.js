fea8c52062f0f6968677792762400fe256bf1a63
root <root@LAPTOP-2LV4F0PM>
2024-05-14 15:52:58 +0200

    title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Invalid response:', response.statusCode);
+    } else {
+        const movie = JSON.parse(body);
+        console.log(movie.title);
+    }
+});
+
