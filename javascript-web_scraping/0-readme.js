dfd0de3c7a34ab65da9f28c55b44b810a3f0fc40
root <root@LAPTOP-2LV4F0PM>
2024-05-14 10:59:39 +0200

    fix variable file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 // Reads and prints the content of a file
 
 
-const argv = process.argv;
+const file = process.argv[2];
 let fs = require('fs');
-fs.readFile(argv[2], 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
