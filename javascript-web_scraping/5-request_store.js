0cfd1bacb3f437338150d025296238b00803dc83
root <root@LAPTOP-2LV4F0PM>
2024-05-15 12:26:42 +0200

    add rappel function

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,23 +1,17 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
+const request = require('request');
+const apiurl = process.argv[2];
 const file = process.argv[3];
 
-request.get(url, function(err, response, body) {
-    if (err) {
-        console.error(err);
-    } else if (response.statusCode !== 200) {
-        console.error('Invalid response:', response.statusCode);
-    } else {
-        fs.writeFile(file, body, { encoding: 'utf-8' }, function(err) {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(`File ${file} has been saved.`);
-            }
-        });
-    }
+request(apiurl, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
