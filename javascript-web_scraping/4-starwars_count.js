b31093496220dc54aa94442da24e9be307c60f59
root <root@LAPTOP-2LV4F0PM>
2024-05-15 16:23:04 +0200

    task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-const Id = '18';
 const apiUrl = process.argv[2];
 
-request.get(apiUrl, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Invalid response:', response.statusCode);
-    } else {
-        const movies = JSON.parse(body).results;
-	    if (!movies) {
-		    console.log('No films found.');
-	    } else {
-		    const moviesWithWedge = movies.filter(movie => movie.characters.includes(`https://swapi-api.hbtn.io/api/people/${Id}/`));
-		    console.log(moviesWithWedge.length);
-	    }
+request.get(apiUrl, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let nboffilms = 0;
+    const films = JSON.parse(body).results;
+    for (let i = 0; i < films.length; i++) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
+          nboffilms += 1;
+        }
+      }
     }
+    console.log(nboffilms);
+  }
 });
-
